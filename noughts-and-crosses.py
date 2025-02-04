ab490fcb91757a7b6e459d2d72c5850e960672d3
Mark Chipperfield <mark-chipperfield@outlook.com>
2025-02-04 19:06:02 +0000

    add main function comment, and change comment format for other functions

--- noughts-and-crosses.py
+++ noughts-and-crosses.py
@@ -8,8 +8,10 @@ board = [
 
 
 def display_board(board):
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
+    """
+    The function accepts one parameter containing the board's current status
+    and prints it out to the console.
+    """
     print(
         f"""
 +-------+-------+-------+
@@ -30,8 +32,10 @@ def display_board(board):
 
 
 def enter_move(board):
-    # The function accepts the board's current status, asks the user about their move,
-    # checks the input, and updates the board according to the user's decision.
+    """
+    The function accepts the board's current status, asks the user about their move,
+    checks the input, and updates the board according to the user's decision.
+    """
     options = make_list_of_free_fields(board)
     num_options = [board[option[0]][option[1]] for option in options]
     user = None
@@ -66,14 +70,18 @@ def enter_move(board):
 
 
 def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares;
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    """
+    The function browses the board and builds a list of all the free squares;
+    the list consists of tuples, while each tuple is a pair of row and column numbers.
+    """
     return [(row, column) for row in range(3) for column in range(3) if board[row][column] != 'O' and board[row][column] != 'X']
 
 
 def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if
-    # the player using 'O's or 'X's has won the game
+    """
+    The function analyzes the board's status in order to check if
+    the player using 'O's or 'X's has won the game
+    """
     for i in range(3):
         col = [board[j][i] for j in range(3)]
         board.append(col)
@@ -91,7 +99,9 @@ def victory_for(board, sign):
 
 
 def draw_move(board):
-    # The function draws the computer's move and updates the board.
+    """
+    The function draws the computer's move and updates the board.
+    """
     options = make_list_of_free_fields(board)
     choice = options[randrange(len(options))]
 
@@ -99,6 +109,9 @@ def draw_move(board):
 
 
 def main(board):
+    """
+    The function runs the game. Prints game outcome message to the terminal.
+    """
     while len(make_list_of_free_fields(board)) != 0:
         display_board(board)
         enter_move(board)
